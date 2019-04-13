29a9dd05671887da5344ea5204917a6694a34029
Radoslaw Sobolak <radoslaw.sobolak@interia.pl>
2019-04-13 22:49:00 +0200

    Add motion detection based on 1 AR marker

---
+++ SamsungLABOProject/Assets/MyData/Scripts/PositionDetectionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionDetectionScript : MonoBehaviour
+{
+
+    public GameObject Plane1;
+
+    public GameObject MainCamera;
+
+    private Vector3 _lastTransform;
+
+	// Use this for initialization
+	void Start () {
+		_lastTransform = new Vector3(0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Plane1.GetComponent<Renderer>().enabled)
+	    {
+	        Vector3 currentTransform = new Vector3(0, 0, 0);
+	        currentTransform -= _lastTransform;
+	        _lastTransform = Plane1.transform.InverseTransformPoint(transform.position);
+	        currentTransform += _lastTransform;
+	        MainCamera.transform.position += currentTransform;
+	    }
+	}
+}
