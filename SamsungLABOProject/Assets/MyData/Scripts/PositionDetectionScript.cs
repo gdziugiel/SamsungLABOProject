fed3cd6ab9dcd91c050f5d4bfaa6b2e549cdab76
Radoslaw Sobolak <radoslaw.sobolak@interia.pl>
2019-04-14 18:45:21 +0200

    Add position tracking for 3 markers

--- SamsungLABOProject/Assets/MyData/Scripts/PositionDetectionScript.cs
+++ SamsungLABOProject/Assets/MyData/Scripts/PositionDetectionScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,8 @@ public class PositionDetectionScript : MonoBehaviour
 {
 
     public GameObject Plane1;
+    public GameObject Plane2;
+    public GameObject Plane3;
 
     public GameObject MainCamera;
 
@@ -18,13 +21,67 @@ public class PositionDetectionScript : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Plane1.GetComponent<Renderer>().enabled)
+	    Vector3 [] currentTransforms = new Vector3[3];
+        bool [] flags = new bool[3];
+
+        if (Plane1.GetComponent<Renderer>().enabled)
 	    {
-	        Vector3 currentTransform = new Vector3(0, 0, 0);
-	        currentTransform -= _lastTransform;
-	        _lastTransform = Plane1.transform.InverseTransformPoint(transform.position);
-	        currentTransform += _lastTransform;
-	        MainCamera.transform.position += currentTransform;
+            currentTransforms[0] -= _lastTransform;
+	        currentTransforms[0] += Plane1.transform.InverseTransformPoint(transform.position);
+
+	        flags[0] = true;
 	    }
+        else
+        {
+            flags[0] = false;
+        }
+        if (Plane2.GetComponent<Renderer>().enabled)
+	    {        
+	        currentTransforms[1] -= _lastTransform;
+	        currentTransforms[1] += Plane2.transform.InverseTransformPoint(transform.position);
+
+	        flags[1] = true;
+	    }
+        else
+        {
+            flags[1] = false;
+        }
+        if (Plane3.GetComponent<Renderer>().enabled)
+	    {
+	        currentTransforms[2] -= _lastTransform;
+	        currentTransforms[2] += Plane3.transform.InverseTransformPoint(transform.position);
+
+	        flags[2] = true;
+	    }
+        else
+        {
+            flags[2] = false;
+        }
+
+        float minMagnitude = float.NaN;
+	    int minMagnitudeIndex = -1;
+	    for (int i = 0; i < currentTransforms.Length; i++)
+	    {
+	        if (flags[i])
+	        {
+	            if (float.IsNaN(minMagnitude))
+	            {
+	                minMagnitude = currentTransforms[i].magnitude;
+	                minMagnitudeIndex = i;
+	            }
+	            else if (currentTransforms[i].magnitude < minMagnitude)
+	            {
+	                minMagnitude = currentTransforms[i].magnitude;
+	                minMagnitudeIndex = i;
+	            }
+	        }
+	    }
+
+	    if (minMagnitudeIndex != -1)
+	    {
+	        MainCamera.transform.position += currentTransforms[minMagnitudeIndex];
+	        _lastTransform = currentTransforms[minMagnitudeIndex] + _lastTransform;
+	    }
+
 	}
 }
